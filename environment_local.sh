87d355b2fc3e32960e01070c4775092666674fa4
Javier Urrutia R <javier.urrutia.r@ug.uchile.cl>
2017-04-05 21:27:56 -0300

    new package para clase ros

---
+++ environment_local.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Activating ROS "$ROS_DISTRO"..."
+source /opt/ros/$ROS_DISTRO/setup.bash
+echo "...done."
+echo
+
+echo "Setting up PYTHONPATH."
+echo "Note: We assume you cloned the Software repository in the folder 'duckietown' at home"
+export PYTHONPATH=$HOME/duckietown/catkin_ws/src:$PYTHONPATH
+echo
+
+echo "Setup ROS_HOSTNAME."
+export ROS_HOSTNAME=localhost
+export DUCKIETOWN_ROOT=$HOME/duckietown
+echo
+
+echo "Building machines file..."
+make -C  $DUCKIETOWN_ROOT
+echo "...done"
+echo
+
+echo "Activating development."
+source $DUCKIETOWN_ROOT/catkin_ws/devel/setup.bash
+
+# TODO: check that the time is >= 2015
+
+# TODO: run a python script that checks all libraries are installed
+
+exec "$@" #Passes arguments. Need this for ROS remote launching to work.
