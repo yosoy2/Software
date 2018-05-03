a0913dc8366e67978191065142375f92d22107bd
EseTP <edotercero66.el@gmail.com>
2018-05-03 18:00:04 -0300

    Se solucionan problemas con la patodeteccion

--- catkin_ws/src/ros_cap/tutorial/nodocamaraseguimientorandomcondetecciondepatodistancia.py
+++ catkin_ws/src/ros_cap/tutorial/nodocamaraseguimientorandomcondetecciondepatodistancia.py
@@ -11,34 +11,40 @@ class Detect(object):
 		super(Detect, self).__init__()
 		self.args = args
           	self.publisher = rospy.Publisher("/duckiebot/Yo_solo_hago_un_cuadradito_al_rededor_de_alguna_wea_amarilla", Image, queue_size=10)
-		self.subscriber = rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
+		self.subscriber = rospy.Subscriber("/duckiebot/camera_node/image/raw", Image, self.callback)
+		self.publisher2 = rospy.Publisher("/duckiebot/Hola",Image, queue_size=10)
+		#self.publisher3 = rospy.Publisher("/duckiebot/Coordenadas", ,)
 		self.image = Image()
 		self.bridge = CvBridge()
 
 	def callback(self,msg):
+		aa = 20
+		bb = 20
 		img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 		imageout = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(imageout,np.array([15,100,0]),np.array([45,255,255]))
+		mask = cv2.inRange(imageout,np.array([0,210,100]),np.array([30,255,200]))
 		kernel = np.ones((5,5),np.uint8)
 		img_out = cv2.erode(mask,kernel,iterations = 1)
+		img_out = cv2.dilate(mask,kernel,iterations = 2)
 		image_out = cv2.bitwise_and(img, img, mask = img_out)
 		hola, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 		areas = [cv2.contourArea(c) for c in contours] #Busca todos los rectangulos en la imagen
 		for a in areas:
-			if a < 1000:
-				i = areas.index(a)
-				areas.remove(a)
-				para_nada_sirvo_pero_aun_asi_doy = contours.pop(i)
-			else: None
-		for a in areas:
 			index = areas.index(a)
 			cnt = contours[index]  #Denomina cnt a los bordes del rectangulo de mayor area
-			if areas[index] < 1000: #and hierarchy[index] == 0:
-				cnt = None
-			x,y,w,h = cv2.boundingRect(cnt)
-			cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,0), 2)
+			if areas[index] < 200 or areas[index] > 3000: #and hierarchy[index] == 0:
+				continue
+			else:
+				x,y,w,h = cv2.boundingRect(cnt)
+				cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,0), 2)
+				#if w<aa or h<bb:
+				#	None
+				#else:
+				print "w = ", w, "h = ", h, "area = ", a
 		image = self.bridge.cv2_to_imgmsg(img,"bgr8")
 		self.publisher.publish(image)
+		image_out = self.bridge.cv2_to_imgmsg(image_out,"bgr8")
+		self.publisher2.publish(image_out)
 
 
 def main():
