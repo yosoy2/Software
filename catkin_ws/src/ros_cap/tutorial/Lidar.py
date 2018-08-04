b0fafd1c91d119743e343202d761de0ceab9ba32
tomn681 <tomasdelasotta@gmail.com>
2018-08-04 18:57:39 -0400

    Se agrega modulo controlador del lidar

---
+++ catkin_ws/src/ros_cap/tutorial/Lidar.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# autores: Pedro y Tomas
+
+import rospy
+
+from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import lidar9
+
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import Joy
+
+# La clase Controller reune la informacion de joy y lidarTopic con el
+# fin de entregar un seguro para la prevencion de colisiones del
+# Duckiebot.
+class Controller(object):
+    def __init__(self, args):
+        super(Controller, self).__init__()
+        
+        self.args = args
+        
+        self.publisher = rospy.Publisher(
+            "/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped,
+            queue_size=10)
+        
+        self.subscriberJoy = rospy.Subscriber("/duckiebot/joy", Joy,
+            self.LecturaJoy)
+        self.subscriberLidar = rospy.Subscriber("/duckiebot/lidarTopic",
+            lidar9, self.LecturaLidar)
+        
+        self.twist = Twist2DStamped()
+
+        self.anguloGiro =
+
+        n = (msg.anglMax-msg.anglMin)/msg.anglDelta + 1
+        self.list = [0]*n
+
+
+    def LecturaJoy(self,msg):
+        self.v = msg.axes[1]*(-1)
+        self.omega = msg.axes[3]
+        if(msg.buttons[1]):
+            self.twist.v = 0
+            self.twist.omega = 0
+
+    
+        
+    def LecturaLidar(self,msg):
+        # Definicion de variables.
+        tiempoBarrido = ((msg.anglMax-msg.anglMin)/msg.anglDelta)
+            * msg.tiempo
+        velocidadMaxima = 
+        indiceDato = (msg.angulo-msg.anglMin)/msg.anglDelta
+        
+
+        # Creacion de la lista de distancias.
+        if self.list is not None:
+            L = self.list
+
+
+        # Actualizacion de lista de distancias.
+        if self.twist.omega == 0:
+            for i in range(L):
+                L[i] = L[i] + self.twist.v * msg.anglDelta     
+        else:
+            self.anguloGiro = self.anguloGiro + self.twist.omega
+            if int(anguloGiro) == 1:
+                L = L.pop(n).append(0)
+                self.anguloGiro = 0
+            elif int(anguloGiro) == -1:
+                L = L.pop(1).reverse().append(0).reverse()
+                self.anguloGiro = 0
+
+
+        # Actualizar datos de distancia.        
+        L[indiceDato] = msg.distancia + self.twist.v * msg.anglDelta 
+        
+
+        # Distancia de objeto mas cercano.
+        indicesCentral = (msg.anglMax - msg.anglMin)/(2 * msg.anglDelta)
+        G = []
+        for indice > indiceCentral - 3 and indice < indiceCentral + 3:
+            G[indice] = L[indice]
+        distanciaMin = min(i in G)
+
+
+        # Freno anticolisiones.
+        if distanciaMin <= V * T:
+            self.twist.v = 0
+
+
+        self.publisher.publish(self.twist)
+        self.list = L
+
+        
+def main():
+    rospy.init_node('test')
+
+    obj = Controller('args')
+
+    rospy.spin()
+
+
+if __name__ =='__main__':
+    main()
