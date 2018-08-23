703897f3cc555931cd2db831b8cde61dfb353b65
yosoy2 <edotercero66.el@gmail.com>
2018-08-23 16:48:48 -0300

    se arregla el nodo del control

--- catkin_ws/src/ros_cap/src/SuperNodoTodoSayayin.py
+++ catkin_ws/src/ros_cap/src/SuperNodoTodoSayayin.py
@@ -12,7 +12,7 @@ class Movimiento(object):
 	def __init__(self, args):
 		super(Movimiento, self).__init__()
 		self.args = args
-          	self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size=10)
+        self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size=10)
 		self.subscriber = rospy.Subscriber("/duckiebot/Control", Twist2DStamped, self.callback)
 		self.suscriber2 = rospy.Subscriber("/duckiebot/PatoUbicacion", Point, self.callback)
 		self.twist = Twist2DStamped()
