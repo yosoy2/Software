47fb49cde6d29d7f9c0e77369ad1e4405fbc472c
yosoy2 <edotercero66.el@gmail.com>
2018-08-23 18:05:57 -0300

    se arregle cambia el nodo controller y se arrgla

--- catkin_ws/src/ros_cap/src/controller.py
+++ catkin_ws/src/ros_cap/src/controller.py
@@ -1,50 +1,49 @@
 #!/usr/bin/env python
 
 import rospy #importar ros para python
+from std_msgs.msg import String, Int32 # importar mensajes de ROS tipo String y tipo Int32
+from geometry_msgs.msg import Twist # importar mensajes de ROS tipo geometry / Twist
 from sensor_msgs.msg import Joy
 from duckietown_msgs.msg import Twist2DStamped
-class controller(object):
-	def __init__(self):
-		super(controller, self).__init__()
-                self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size = 1)
-                self.subscriber = rospy.Subscriber("/duckiebot/joy", Joy, self.callback)
-                self.twist = Twist2DStamped()
-
-
+ 
+class Control(object):
+	def __init__(self, args):
+		super(Control, self).__init__()
+		self.args = args
+		self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size = 0)
+		self.subscriber = rospy.Subscriber("/duckiebot/joy", Joy,self.callback)
+		self.twist = Twist2DStamped()
 
 	#def publicar(self):
 
+	#def callback(self,msg):
 	def callback(self,msg):
-	#rospy.loginfo(msg.axes)
-                self.twist.v = 0
-                self.twist.omega = 0
-		if msg.buttons[0] == 1:
-			self.twist.v=10
-			print("a")
-                elif msg.buttons[1] == 1:
-			self.twist.v=-10
-			print("b")
-		elif msg.buttons[11]==-1:
-                        self.twist.omega=-10
-			print("<-")
-		elif msg.buttons[12]==1:
-                        self.twist.omega=10
-			print("->")	
-		print self.twist	
-		self.publisher.publish(self.twist)
-	
-
+		angular = msg.axes[0]*6
+		velocidad = msg.axes[1]*0.4
+		self.twist.v = velocidad
+		self.twist.omega = angular
+		if(msg.buttons[1] == 1): #B frena
+			self.twist.v = 0
+			self.twist.omega = 0
+			self.publisher.publish(self.twist)
+		else:
+			if(msg.buttons[0] == 1): 
+				self.publisher.publish(self.twist)
+		
 
 
 
 def main():
-	rospy.init_node('test') #creacion y registro del nodo!
+	rospy.init_node('control') #creacion y registro del nodo!
 
-	obj = controller() # Crea un objeto del tipo Template, cuya definicion se encuentra arriba
+	obj = Control('args') # Crea un objeto del tipo Template, cuya definicion se encuentra arriba
+	
 
 	#objeto.publicar() #llama al metodo publicar del objeto obj de tipo Template
+	
 
-	rospy.spin() #funcion de ROS que evita que el programa termine -  se debe usar en  Subscribers
+	#rospy.spin() #funcion de ROS que evita que el programa termine -  se debe usar en  Subscribers
+	rospy.spin()
 
 
 if __name__ =='__main__':
