4305d373920aced7aebcd24c6fd00977df94507f
yosoy2 <edotercero66.el@gmail.com>
2018-08-23 17:08:57 -0300

    se vuelve a añadir el controller

---
+++ catkin_ws/src/ros_cap/src/controller.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# autores: eduardo y tomas
+
+import rospy #importar ros para python
+ # importar mensajes de ROS tipo String y tipo Int32 # importar mensajes de ROS tipo geometry / Twist
+from sensor_msgs.msg import Joy
+from duckietown_msgs.msg import Twist2DStamped
+
+class Controller(object):
+	
+	def __init__(self, args):
+		super(Controller, self).__init__()
+		self.args = args
+        self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size=10)
+        self.publisher = rospy.Publisher("/duckiebot/Control", Twist2DStamped, queue_size=10)
+		self.subscriber = rospy.Subscriber("/duckiebot/joy", Joy, self.callback)
+		self.twist = Twist2DStamped()
+		self.list = []
+
+	def callback(self,msg):
+        num = msg.axes[1]
+		self.list[i]=msg.distancia		
+  		self.twist.v=msg.axes[1]*(-1)
+		self.twist.omega=msg.axes[3]*10
+		if(msg.buttons[1]):
+			self.twist.v=0
+			self.twist.omega=0
+			self.publisher.publish(self.twist)
+
+def main():
+	rospy.init_node('test') #creacion y registro del nodo!
+
+	obj = Controller('args') # Crea un objeto del tipo Controller, cuya definicion se encuentra arriba
+
+	#objeto.publicar() #llama al metodo publicar del objeto obj de tipo Template
+
+	rospy.spin() #funcion de ROS que evita que el programa termine -  se debe usar en  Subscribers
+
+
+if __name__ =='__main__':
+	main()
