d2c7a0ed6d39511c8648cb6aaa547cdef037b038
yosoy2 <edotercero66.el@gmail.com>
2018-08-23 17:51:27 -0300

    se modifica el controller desde el repositorio de peiro

--- catkin_ws/src/ros_cap/src/controller.py
+++ catkin_ws/src/ros_cap/src/controller.py
@@ -1,36 +1,46 @@
 #!/usr/bin/env python
-# autores: eduardo y tomas
 
 import rospy #importar ros para python
- # importar mensajes de ROS tipo String y tipo Int32 # importar mensajes de ROS tipo geometry / Twist
 from sensor_msgs.msg import Joy
 from duckietown_msgs.msg import Twist2DStamped
+class controller(object):
+	def __init__(self):
+		super(controller, self).__init__()
+                self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size = 1)
+                self.subscriber = rospy.Subscriber("/duckiebot/joy", Joy, self.callback)
+                self.twist = Twist2DStamped()
 
-class Controller(object):
-	
-	def __init__(self, args):
-		super(Controller, self).__init__()
-		self.args = args
-        self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size=10)
-        self.publisher = rospy.Publisher("/duckiebot/Control", Twist2DStamped, queue_size=10)
-		self.subscriber = rospy.Subscriber("/duckiebot/joy", Joy, self.callback)
-		self.twist = Twist2DStamped()
-		self.list = []
+
+
+	#def publicar(self):
 
 	def callback(self,msg):
-        num = msg.axes[1]
-		self.list[i]=msg.distancia		
-  		self.twist.v=msg.axes[1]*(-1)
-		self.twist.omega=msg.axes[3]*10
-		if(msg.buttons[1]):
-			self.twist.v=0
-			self.twist.omega=0
-			self.publisher.publish(self.twist)
+	#rospy.loginfo(msg.axes)
+                self.twist.v = 0
+                self.twist.omega = 0
+		if msg.buttons[0] == 1:
+			self.twist.v=10
+			print("a")
+                elif msg.buttons[1] == 1:
+			self.twist.v=-10
+			print("b")
+		elif msg.buttons[11]==-1:
+                        self.twist.omega=-10
+			print("<-")
+		elif msg.buttons[12]==1:
+                        self.twist.omega=10
+			print("->")	
+		print self.twist	
+		self.publisher.publish(self.twist)
+	
+
+
+
 
 def main():
 	rospy.init_node('test') #creacion y registro del nodo!
 
-	obj = Controller('args') # Crea un objeto del tipo Controller, cuya definicion se encuentra arriba
+	obj = controller() # Crea un objeto del tipo Template, cuya definicion se encuentra arriba
 
 	#objeto.publicar() #llama al metodo publicar del objeto obj de tipo Template
 
@@ -38,4 +48,4 @@ def main():
 
 
 if __name__ =='__main__':
-	main()
+main()
